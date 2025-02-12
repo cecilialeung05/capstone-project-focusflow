9d6e940a776443cbff07ee74cde32238df2f8597
Cecilia Leung <Cecilialeung05@gmail.com>
2025-02-11 19:16:07 -0500

    run notes and tasks migrations

---
+++ focusflow/server/migrations/20250211235941_create_tasks_table.js
@@ -0,0 +1,24 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export function up(knex) {
+    return knex.schema.createTable('tasks', (table) => {
+      table.increments('id').primary(); // Primary Key
+      table.string('title').notNullable();
+      table.text('description'); // More space for detailed descriptions
+      table.enu('status', ['open', 'in progress', 'completed', 'blocked']).defaultTo('open'); // Enum for task status
+      table.date('due_date'); // Optional due date
+      table.integer('priority'); // Numerical priority, higher value means higher priority.
+      table.timestamp('created_at').defaultTo(knex.fn.now());
+      table.timestamp('updated_at').defaultTo(knex.raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
+    });
+  };
+  
+  /**
+   * @param { import("knex").Knex } knex
+   * @returns { Promise<void> }
+   */
+  export function down(knex) {
+    return knex.schema.dropTable('tasks');
+  };
---
+++ focusflow/server/migrations/20250212000556_create_notes_table.js
@@ -0,0 +1,27 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export function up(knex) {
+    return knex.schema.createTable('notes', (table) => {
+      table.increments('id').primary(); // Primary Key
+      table
+        .integer('task_id')
+        .unsigned()
+        .references('tasks.id')
+        .onUpdate('CASCADE')
+        .onDelete('CASCADE'); // Foreign Key to tasks
+      table.string('title').notNullable();
+      table.text('content').notNullable(); // Store the actual note content.  Use 'text' for larger notes
+      table.timestamp('created_at').defaultTo(knex.fn.now());
+      table.timestamp('updated_at').defaultTo(knex.raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
+    });
+  };
+  
+  /**
+   * @param { import("knex").Knex } knex
+   * @returns { Promise<void> }
+   */
+  export function down(knex) {
+    return knex.schema.dropTable('notes');
+  };
