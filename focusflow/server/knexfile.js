af2b914d8cae51a19a303f4116d14441de37e000
Cecilia Leung <Cecilialeung05@gmail.com>
2025-02-18 05:16:41 -0500

    Create knexfile.js

---
+++ focusflow/server/knexfile.js
@@ -0,0 +1,15 @@
+module.exports = {
+    client: 'mysql2',
+    connection: {
+      host: '127.0.0.1',
+      port: 8080,
+      user: 'root',
+      password: 'rootroot',
+      database: 'focusflow'
+    },
+    migrations: {
+      directory: './migrations'
+    seeds: {
+      directory: './seeds'
+    }
+  };
